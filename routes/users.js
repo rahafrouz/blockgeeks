6f7022c28be1187eedc88c907a9faee84069ea6a
AmiR <rahafrouz@gmail.com>
2019-05-07 21:39:36 +0200

    added db support

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+	const mongoose = require('mongoose');
+	mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
+
+	const Cat = mongoose.model('Cat', { name: String });
+
+	const kitty = new Cat({ name: 'Zildjian' });
+	kitty.save().then(() => console.log('meow'));
+
   res.send('respond with a resource');
 });
 
