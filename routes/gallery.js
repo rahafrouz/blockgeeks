189bdfca557152958e713932909f5db5b1cf9e25
Meruert Nurgazy <meru.nurgazy@gmail.com>
2019-05-07 23:27:31 +0200

    gallery pics rendered from publishedImages folder

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+var picIds = ["img_10.jpg", "img_4.jpg"]; 
+var sourceFolder = "/publishedImages/";
 router.get('/', function(req, res) {
-  res.render('gallery', { title: "gallery" });
+  res.render('gallery', { sourceFolder: sourceFolder, picIds: picIds });
 });
 
 module.exports = router;
