da370f8d401f2941ebfdf4b54fc60bdff3614fa0
Meruert Nurgazy <meru.nurgazy@gmail.com>
2019-05-08 16:29:02 +0200

    gallery search input init

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var picIds = ["img_10.jpg", "img_4.jpg"]; 
-var sourceFolder = "/publishedImages/";
+
 router.get('/', function(req, res) {
-  res.render('gallery', { sourceFolder: sourceFolder, picIds: picIds });
+  res.render('gallery');
 });
 
 module.exports = router;
